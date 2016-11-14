f17de89fb12d26509276e12168d058773c4aae7a
Tim Glatthard <tim@tims-mbp.bfh.ch>
2016-11-14 15:10:41 +0100

    no message

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>tim war zuletzt</title>
+    <title>tim war zuletztasdfaf</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
