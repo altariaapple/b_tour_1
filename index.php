bc85c4d4cda941665b87d7ccb76420d905d77843
Tim Glatthard <tim@tims-mbp.bfh.ch>
2016-11-15 11:52:23 +0100

    Merge branch 'master' of https://github.com/altariaapple/b_tour_1
    # Conflicts:
    #	index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Photoloc</title>
+    <title>Photoloc!</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
