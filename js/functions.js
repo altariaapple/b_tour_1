01ba6d65c322d73f5bc6d3bcdf813d8935fa45b0
Tim Glatthard <tim@tims-mbp.bfh.ch>
2016-11-15 10:36:59 +0100

    no message

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,11 @@
 /*   b_Tour_1 javascript  */
 /* Tim Glatthard, Carmina Grünig, Joel Viotti */
 
-
-<<<<<<< HEAD
 /* ---------- index.php ----------- */
 /* -------------------------------- */
 
 
 /* Button-group Anreise, jeweiligen Text anzeigen */
-=======
 /*Profil.php Anzeigen Inhalte, je nach  Buttons */
 
 $(document).ready(function() {
@@ -19,7 +16,6 @@ $(document).ready(function() {
 
 });
 
->>>>>>> 18e659899c682948a362edcbc26e4bc5c999af13
 $(document).ready(function() {
     $('button').click(function(event) {
     	/* die var pickedOption beinhaltet die jeweilige ID des Buttons, auf welchen geklickt wurde */
