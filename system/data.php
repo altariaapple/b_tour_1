1d3cca1e65d6991cb0f2ce860428020d7e8dd843
Carmina Lena <carminalena@carminas-mbp.bfh.ch>
2016-11-24 11:13:35 +0100

    no message

--- system/data.php
+++ system/data.php
@@ -16,7 +16,7 @@
   function get_result($sql)
   {
     $db = get_db_connection();
-    echo $sql ."<br>";
+    //echo $sql ."<br>";
     $result = mysqli_query($db, $sql);
     mysqli_close($db);
     return $result;
@@ -46,7 +46,7 @@
  /* ----------------------- */
 
  function get_all_pictures(){
-   $sql = "SELECT * FROM picture ORDER BY timestamp DESC;";
+   $sql = "SELECT * FROM picture";
    return get_result($sql);
  }
 
@@ -79,9 +79,8 @@
     return get_result($sql);
   }
 
-  function update_like($picID, $userID){
+  function update_like($picID,$userID){
     $sql = "INSERT INTO likes (user_id, picture_id) VALUES ('$userID', '$picID');";
-    return get_result($sql);
   }
   /* ----------------------------------------------------------------------------- */
 
@@ -114,7 +113,7 @@
   /* ---- profil.php ---- */
   /* ----------------------- */
   // userdaten updaten
-  function update_user($firstname, $lastname, $email, $password, $user_id){
+  function update_user($firstname, $lastname, $email, $password, $confirm_password, $user_id){
       $sql_ok = false;
       $sql = "UPDATE user SET ";
     //   if($profilfoto != ""){ //wenn profilfoto nicht leer dann:
@@ -122,23 +121,22 @@
     //     $sql_ok = true;
     // }
 
-      if($firstname != ""){ //wenn profilfoto nicht leer dann:
-        $sql .= "first_name = '$firstname', ";  //vorhandener string wird erweitert
+      if($firstname != ""){
+        $sql .= "first_name = '$firstname', ";
         $sql_ok = true;
       }
 
-      if($lastname != ""){ //wenn profilfoto nicht leer dann:
-        $sql .= "last_name = '$lastname', ";  //vorhandener string wird erweitert
+      if($lastname != ""){
+        $sql .= "last_name = '$lastname', ";
         $sql_ok = true;
       }
 
-      if($email != ""){ //wenn profilfoto nicht leer dann:
-        $sql .= "email = '$email', ";  //vorhandener string wird erweitert
-        $sql_ok = true;
+      if($email != ""){
+        $sql .= "email = '$email', ";
       }
 
-      if($password != "" && $confirm_password == $password){ //wenn profilfoto nicht leer dann:
-        $sql .= "password = '$password', ";  //vorhandener string wird erweitert
+      if($password != "" && $confirm_password == $password){
+        $sql .= "password = '$password', ";
         $sql_ok = true;
       }
 
@@ -153,23 +151,4 @@
         return false;
       }
 }
-
-function get_friend_list($user_id){
-    $sql = "SELECT * FROM user WHERE user_id in
-              (SELECT user_id_followed FROM follower2 WHERE user_id_follower = $user_id)
-              AND  NOT user_id = $user_id;";
-		return get_result($sql);
-	}
-
- function freund_hinzufuegen(){
-   $sql = "INSERT INTO follower2 (`user_id_follower`, `user_id_followed`) VALUES ($user_id, $get_klicked_picture_owner);";
-   return get_result($sql);
-}
-
-function remove_friends($user_id, $friend_list){
-   $sql = "DELETE FROM follower2 WHERE user = $user_id AND friend = $friend_id;";
-   get_result($sql);
- }
-
-
   ?>
